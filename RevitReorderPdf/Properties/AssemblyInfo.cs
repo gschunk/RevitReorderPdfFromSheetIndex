2ed36502dc8e0806d131b80b620bfeb2c848e528
Greg Schunk <greg@pheinex.com>
2018-07-26 14:23:15 -0500

    Add project files.

---
+++ RevitReorderPdf/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RevitReorderPdf")]
+[assembly: AssemblyDescription("Revit Add-In Description for RevitReorderPdf")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Pheinex, LLC.")]
+[assembly: AssemblyProduct("RevitReorderPdf Revit Add-In")]
+[assembly: AssemblyCopyright("Copyright 2016 © Greg Schunk Pheinex LLC.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("E3F40D91-3D89-4E27-9157-83CBA6AFA346")]
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2016.0.0.0")]
+[assembly: AssemblyFileVersion("2016.0.0.0")]
